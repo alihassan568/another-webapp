82cacfd54b20802028bd3cbc7a263862d7686b96
Ali Hassan <bc230419312ira@vu.edu.pk>
2025-12-06 01:31:28 +0500

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        
         User::factory()->create([
             'name' => 'Super Admin',
             'email' => 'admin@anothergo.com',
